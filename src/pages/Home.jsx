dc555287c0843046ce187b8baf34a243f8845ca2
tamudadi <177226849+UserTamura@users.noreply.github.com>
2025-05-18 19:05:33 +0900

    ローカルデータで投稿リスト表示機能の動作検証

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,39 @@
 import React from 'react';
-
-// const dummyPost = {
-//   id: 1,
-//   title: '記事タイトル１',
-//   thumbnailUrl: 'https://placehold.jp/800x400.png',
-//   createdAt: '2023-09-11T09:00:00.000Z',
-//   categories: ['React', 'TypeScript'],
-//   content: `
-//     本文です。本文です。本文です。本文です。本文です。本文です。<br/>本文です。本文です。本文です。本文です。本文です。<br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。本文です。<br/><br/><br/>本文です。本文です。本文です。本文です。本文です。本文です。<br/>`,
-// };
+import { posts } from '../data/posts';
 
 export const Home = () => {
   return (
     <div>
       <div className="container my-10 mx-auto px-4">
         <ul>
-          <li className="list flex flex-col text-[#333]">
-            <div className="post mb-8 border p-4 border-gray-300">
-              <div className="postInfo flex justify-between">
-                <div className="postDate !text-xs !text-[#888]">2025/5/17</div>
-                <div className="postCategories border rounded px-1.5 py-1 text-sm text-[#06C] border-[#06C] mr-2">
-                  React
+          {posts.map((post) => (
+            <li key={post.id} className="list flex flex-col text-[#333]">
+              <div className="post mb-8 border p-4 border-gray-300">
+                <div className="postInfo flex justify-between">
+                  <div className="postDate !text-xs !text-[#888]">
+                    {new Date(post.createdAt).toLocaleDateString()}
+                  </div>
+                  <div className="postCategories flex">
+                    {post.categories.map((category) => (
+                      <div
+                        key={category}
+                        className="border rounded px-1.5 py-1 text-sm text-[#06C] border-[#06C] mr-2"
+                      >
+                        {category}
+                      </div>
+                    ))}
+                  </div>
                 </div>
+                <p className="postTitle text-2xl mt-2 mb-4">
+                  {`APIで取得した${post.title}`}
+                </p>
+                <div
+                  className="postBody line-clamp-2"
+                  dangerouslySetInnerHTML={{ __html: post.content }}
+                />
               </div>
-              <h3 className="postTitle text-2xl mt-2 mb-4">
-                APIで取得した記事タイトル1
-              </h3>
-              <p className="postBody line-clamp-2">
-                ほんぶんほんぶんほんぶんほんぶんほんぶんほんぶんほんぶんほんぶんほんぶんほんぶんほんぶんほんぶん
-              </p>
-            </div>
-          </li>
-          <li className="list flex flex-col text-[#333]">
-            <div className="post mb-8 border p-4 border-gray-300">
-              <div className="postInfo flex justify-between">
-                <div className="postDate !text-xs !text-[#888]">2025/5/17</div>
-                <div className="postCategories border rounded px-1.5 py-1 text-sm text-[#06C] border-[#06C] mr-2">
-                  React
-                </div>
-              </div>
-              <h3 className="postTitle text-2xl mt-2 mb-4">
-                APIで取得した記事タイトル1
-              </h3>
-              <p className="postBody line-clamp-2">
-                ほんぶんほんぶんほんぶんほんぶんほんぶんほんぶんほんぶんほんぶんほんぶんほんぶんほんぶんほんぶん
-              </p>
-            </div>
-          </li>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
