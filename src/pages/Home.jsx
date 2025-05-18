c5b45a7b77093c0b496111661d53216e77bf363b
tamudadi <177226849+UserTamura@users.noreply.github.com>
2025-05-18 19:15:14 +0900

    現時点で不要なCSSの削除

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,13 +7,13 @@ export const Home = () => {
       <div className="container my-10 mx-auto px-4">
         <ul>
           {posts.map((post) => (
-            <li key={post.id} className="list flex flex-col text-[#333]">
-              <div className="post mb-8 border p-4 border-gray-300">
-                <div className="postInfo flex justify-between">
-                  <div className="postDate !text-xs !text-[#888]">
+            <li key={post.id} className="flex flex-col text-[#333]">
+              <div className="mb-8 border p-4 border-gray-300">
+                <div className="flex justify-between">
+                  <div className="text-xs text-[#888]">
                     {new Date(post.createdAt).toLocaleDateString()}
                   </div>
-                  <div className="postCategories flex">
+                  <div className="flex">
                     {post.categories.map((category) => (
                       <div
                         key={category}
@@ -24,11 +24,11 @@ export const Home = () => {
                     ))}
                   </div>
                 </div>
-                <p className="postTitle text-2xl mt-2 mb-4">
+                <p className="text-2xl mt-2 mb-4">
                   {`APIで取得した${post.title}`}
                 </p>
                 <div
-                  className="postBody line-clamp-2"
+                  className="line-clamp-2"
                   dangerouslySetInnerHTML={{ __html: post.content }}
                 />
               </div>
